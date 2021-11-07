2aa9ae0774a905b81e7088e3391cf8ddfe22547f
Marius Gurgu <marius.gurgu97@gmail.com>
2021-11-07 14:09:08 +0200

    Updated audio script file to run on raspi

--- embedded_code/audioscript.py
+++ embedded_code/audioscript.py
@@ -1,7 +1,8 @@
 import pyaudio
 import wave
 import ntplib
-from main import ctime
+import time
+
 form_1 = pyaudio.paInt16 # 16-bit resolution
 chans = 1 # 1 channel
 samp_rate = 44100 # 44.1kHz sampling rate
@@ -11,19 +12,25 @@ dev_index = 2 # device index found by p.get_device_info_by_index(ii)
 wav_output_filename = 'test1.wav' # name of .wav file
 
 audio = pyaudio.PyAudio() # create pyaudio instantiation
+
 #method for getting the current date and time
 def get_filename():
-    ntp_client = ntplib.NTPClient()
-    response = ntp_client.request('pool.ntp.org')
-    time = ctime(response.tx_time)
-    array = time.split(" ")
-    time = array[4] + "/" + array[3] + ":" + array[1] + ":" + array[5]
-    return time
+    # ntp_client = ntplib.NTPClient() 
+    # response = ntp_client.request('pool.ntp.org')
+    #use local time, otherwise timeout from server will terminate program
+    timestamp = time.ctime()
+    array = timestamp.split(" ")
+    timestamp = array[4] + "_" + array[3] + "-" + array[1] + "-" + array[5]
+    return timestamp
 # create pyaudio stream
+
 stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
                     input_device_index = dev_index,input = True, \
                     frames_per_buffer=chunk)
+
+chunk_counter = 0
 print("recording")
+
 while 1>0:
     frames = []
 
@@ -38,11 +45,20 @@ while 1>0:
     stream.stop_stream()
     stream.close()
     audio.terminate()
-    wav_output_filename=get_filename()+".wav"
+    print(get_filename())
+    print(wav_output_filename)
+    wav_output_filename= str(get_filename())+".wav"
     # save the audio frames as .wav file
-    wavefile = wave.open(wav_output_filename,'wb')
+    wavefile = wave.open('recordings/' + wav_output_filename,'wb')
     wavefile.setnchannels(chans)
     wavefile.setsampwidth(audio.get_sample_size(form_1))
     wavefile.setframerate(samp_rate)
     wavefile.writeframes(b''.join(frames))
     wavefile.close()
+    print("Saved chunk " + str(chunk_counter) + ':  ' + wav_output_filename )
+    chunk_counter += 1
+
+    audio = pyaudio.PyAudio() # create pyaudio instantiation
+    stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
+                    input_device_index = dev_index,input = True, \
+                    frames_per_buffer=chunk)
