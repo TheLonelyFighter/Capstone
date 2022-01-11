c6b4f03638ffa96a3d3afff33809dd242843e7b1
Atte Rehnbäck <aerehn@utu.fi>
2022-01-11 18:58:18 +0200

    prototype ready for testing

---
+++ Test_frame/KNN_model/model_training.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import math as math
+import os
+import joblib
+from sklearn.neighbors import KNeighborsClassifier
+cwd = os.getcwd()
+files = os.listdir(cwd)
+print("Files in %r: %s" % (cwd, files))
+data_path = cwd+"/Test_frame/KNN_model/DataFrame_01_12.csv"
+data = pd.read_csv(data_path)
+
+def getSamplesAndLabels(data,variables,label):
+    x=[]
+    y=[]
+    for row in range(len(data)):
+        sample=[]
+        nan=False
+        for variable in variables:
+            point = data[variable][row]
+            sample.append(data[variable][row])
+            if math.isnan(point):
+                nan=True
+        if(not nan):
+            x.append(sample)
+            y.append(data[label][row])
+        
+    return x,y
+
+variables = ["RMS","RMS_filt","Zero-Cross","Zero-Cross_filt","Spectral_centroid","Spectral_centroid_filt","Spectral_slope","Spectral_slope_filt","Spectral_spread","Spectral_spread_filt","STD","STD_filt"]
+
+X,y=getSamplesAndLabels(data,variables,"Surface")
+
+knn=KNeighborsClassifier(n_neighbors=3)
+knn.fit(X,y)
+
+joblib.dump(knn,cwd+'/Test_frame/KNN_model/'+'knn_model.pkl')
--- Test_frame/demo_main.py
+++ Test_frame/demo_main.py
@@ -1,9 +1,12 @@
 import pyaudio
 import numpy as np
-
+import os
+import joblib
+from preprocessing.get_features import get_features
+from sklearn.neighbors import KNeighborsClassifier
 
 def main():
-    AUDIO_WINDOW_LENGTH=3
+    AUDIO_WINDOW_LENGTH=10
     form_1 = pyaudio.paInt16 # 16-bit resolution
     chans = 1 # 1 channel
     samp_rate = 44100 # 44.1kHz sampling rate
@@ -11,8 +14,11 @@ def main():
     record_secs = 1 # seconds to record
     #dev_index = 2 # device index found by p.get_device_info_by_index(ii)
     p = pyaudio.PyAudio()
-    
-    dev_index = 15
+    cwd = os.getcwd()
+    #files = os.listdir(cwd)
+    #print(files)
+    knn_classifier=joblib.load(cwd+'/Test_frame/KNN_model/knn_model.pkl' , mmap_mode ='r')
+    dev_index = 1
     print("Using device",p.get_device_info_by_index(dev_index))
     wav_output_filename = 'test1.wav' # name of .wav file
 
@@ -27,11 +33,11 @@ def main():
     print("recording")
     recordings=[]
     test=[]
-    i=10
+    i=20
     stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
                         input_device_index = dev_index,input = True, \
                         frames_per_buffer=chunk)
-    while i>0:
+    while 1>0:
         
         frames = []
 
@@ -40,18 +46,29 @@ def main():
             data = stream.read(chunk)
             frames.append(data)
         joined_frame=b''.join(frames)
-        sound_data = np.frombuffer(data, dtype=np.int16)
+        sound_data = np.frombuffer(joined_frame, dtype=np.int16)
         recordings.append(sound_data)
         test.append("test"+str(i))
         if(len(recordings)>AUDIO_WINDOW_LENGTH/record_secs):
             recordings.pop(0)
             test.pop(0)
-        # stop the stream, close it, and terminate the pyaudio instantiation
+        # stop the stream, close it, and terminate the pyaudio instance
         #concatenate the audio samples and extract features
+        #big_chungus is a large audiosample in as a numpy array
+        big_chungus = np.concatenate(recordings)
+        #print(big_chungus)
+        
+        #extract features using tsfel and other libraries
+        features = get_features(big_chungus)
+        print("features:",features)
         #Use knn to classify the audiosamples and other features i.e. temps, humidity, speed etc.
+        predicted_road_type=knn_classifier.predict([features])
         #print the road condition
-        print(test)
-        print(len(recordings))
+        print(predicted_road_type[0])
+        #print(test)
+        #print(len(recordings))
+        #print(recordings[0])
+        #print(len(recordings[-1]))
         i=i-1
     stream.stop_stream()
     stream.close()
