b12affa215d28972da6456ab7b68c4e9215bdc29
Jan Böhmeke <bohmeke.jan@gmail.com>
2022-02-11 18:28:46 +0200

    updated version

--- guess_the_road_condition/quiz_game_road.py
+++ guess_the_road_condition/quiz_game_road.py
@@ -70,14 +70,14 @@ class Quiz:
     def buttons(self):
 
         next_button = Button(gui, text="Next", command=self.next_btn,
-                             width=10, bg="cyan", fg="black", font=("ariel", 16, "bold"))
+                             width=10, bg="cyan", fg="black", font=("ariel", 20, "bold"))
 
-        next_button.place(x=350, y=380)
+        next_button.place(x=300, y=380)
 
         quit_button = Button(gui, text="Quit", command=gui.destroy,
                              width=5, bg="cyan", fg="black", font=("ariel", 16, " bold"))
 
-        quit_button.place(x=720, y=720)
+        quit_button.place(x=700, y=700)
 
     def display_options(self):
         val = 0
@@ -93,27 +93,42 @@ class Quiz:
         q_no = Label(gui, text=question[self.q_no], width=60,
                      font=('ariel', 18, 'bold'), anchor='w')
 
-        q_no.place(x=70, y=100)
+        q_no.place(x=70, y=200)
 
     def display_title(self):
 
-        title = Label(gui, text="Guess The Road Condition!  By: Ice Riderzzz",
+        title1 = Label(gui, text="Guess The Road Condition!  By: Ice Riderzzz",
                       width=50, bg="cyan", fg="black", font=("ariel", 20, "bold"))
 
-        title.place(x=0, y=2)
+        title1.place(x=0, y=2)
+
+
+        title2 = Label(gui, text=r"https://github.com/TheLonelyFighter/Capstone",
+                      width=50, bg="white", fg="black", font=("Courier", 10, "italic"))
+
+        title2.place(x=0, y=600)
+
+        """ def callback(event):
+        webbrowser.open_new(event.widget.cget("text")) """
 
     def radio_buttons(self):
 
         q_list = []
-        y_pos = 150
+        y_pos = 270
+        x_pos = 140
         while len(q_list) < 3:
             radio_btn = Radiobutton(gui, text=" ", variable=self.opt_selected,
                                     value=len(q_list)+1, font=("ariel", 14))
 
             q_list.append(radio_btn)
             # print(len(q_list)) #debug
-            radio_btn.place(x=100, y=y_pos)
-            y_pos += 40
+            radio_btn.place(x=x_pos, y=y_pos)
+            if len(q_list) == 1:
+                x_pos += 100
+                print(len(q_list))
+            elif len(q_list) == 2:
+                x_pos += 150
+                print(len(q_list))
         return q_list
 
 
@@ -130,21 +145,13 @@ with open('json_for_quiz_game.json') as f:
     data = json.load(f)
 
 
-""" def unpause():
+def unpause():
     pygame.mixer.music.unpause()
 
 
 def pause():
     pygame.mixer.music.pause()
 
-button4 = Button(gui, text="Continue", command=unpause, width=15)
-button4.pack(pady=5)
-button4.place(x=520, y=180)
-
-button5 = Button(gui, text="Pause", command=pause, width=15)
-button5.pack(pady=5)
-button5.place(x=520, y=220) 
- """
 
 question = (data['question'])
 options = (data['options'])
@@ -170,26 +177,38 @@ def play_dry_asphalt():
     pygame.mixer.music.play()
 
 
-button1 = Button(gui, text="First Sound To Guess",
-                 command=play_gravel, width=20)
+
+
+button1 = Button(gui, text="Sound 3",
+                 command=play_dry_asphalt, width=20)
 button1.pack(pady=5)
-button1.place(x=340, y=160)
-button2 = Button(gui, text="Second Sound To Guess",
+button1.place(x=460, y=100)
+button2 = Button(gui, text="Sound 2",
                  command=play_wet_asphalt, width=20)
 button2.pack(pady=10)
-button2.place(x=340, y=200)
-button3 = Button(gui, text="Third Sound To Guess",
-                 command=play_dry_asphalt, width=20)
+button2.place(x=300, y=100)
+button3 = Button(gui, text="Sound 1",
+                 command=play_gravel, width=20)
 button3.pack(pady=15)
-button3.place(x=340, y=240)
+button3.place(x=140, y=100)
 
 
+button4 = Button(gui, text="Continue", command=unpause, width=15)
+button4.pack(pady=5)
+button4.place(x=240, y=70)
+
+button5 = Button(gui, text="Pause", command=pause, width=15)
+button5.pack(pady=5)
+button5.place(x=360, y=70)
+
+
+gui.configure(background='grey')
 gui.mainloop()
 
 
 
 #TODO 
-""" Tkinter or sqlite3 for data collection,
+""" Pickle or sqlite3 for data collection,
 change GUI friendlier """
 
 
